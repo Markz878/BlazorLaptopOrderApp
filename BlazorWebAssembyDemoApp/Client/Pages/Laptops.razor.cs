362ce911b69d906c0b1511917f7a47eaf233bf7e
Markus Nisula <markus_nisula@hotmail.com>
2020-07-28 14:15:37 +0300

    Commit

--- BlazorWebAssembyDemoApp/Client/Pages/Laptops.razor.cs
+++ BlazorWebAssembyDemoApp/Client/Pages/Laptops.razor.cs
@@ -7,10 +7,11 @@ using System;
 using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Collections.Generic;
 
 namespace BlazorWebAssembyDemoApp.Client.Pages
 {
-    [Authorize]
     public partial class Laptops
     {
 
@@ -23,11 +24,23 @@ namespace BlazorWebAssembyDemoApp.Client.Pages
         public UserModel User { get; set; } = new UserModel();
         public bool[] CompareList { get; set; }
         PageState State { get; set; } = PageState.Listing;
+        SortMethods SortMethod { get; set; } = SortMethods.None;
+        public bool SortAscending { get; set; }
         enum PageState
         {
             Listing,
             Comparing,
             Ordered,
+        }
+
+        enum SortMethods
+        {
+            None,
+            Price,
+            Rating,
+            Name,
+            Memory,
+            Ram
         }
 
         protected async override Task OnInitializedAsync()
@@ -42,9 +55,9 @@ namespace BlazorWebAssembyDemoApp.Client.Pages
             {
                 httpClient.DefaultRequestHeaders.Add("Authorization", $"Bearer {token.Value}");
                 var laptopList = await httpClient.GetJsonAsync<LaptopModel[]>("api/laptops");
-                LaptopList = laptopList.Select(x => new LaptopCompareModel() { Id = x.Id, Name = x.Name, Manufacturer = x.Manufacturer, GPU = x.GPU, ImageUrl = x.ImageUrl, Memory = x.Memory, Price = x.Price, Processor = x.Processor, Ram = x.Ram, Rating=x.Rating }).ToArray();
+                LaptopList = laptopList.Select(x => new LaptopCompareModel() { Id = x.Id, Name = x.Name, Manufacturer = x.Manufacturer, GPU = x.GPU, ImageUrl = x.ImageUrl, Memory = x.Memory, Price = x.Price, Processor = x.Processor, Ram = x.Ram, Rating = x.Rating }).ToArray();
                 var manufacturers = await httpClient.GetJsonAsync<ManufacturerModel[]>("api/manufacturers");
-                Manufacturers = manufacturers.Select(x => new ManufacturerViewModel() { Id = x.Id, Name = x.Name, ImageUrl = x.ImageUrl, CSSClass= "manufacturer-image" }).ToArray();
+                Manufacturers = manufacturers.Select(x => new ManufacturerViewModel() { Id = x.Id, Name = x.Name, ImageUrl = x.ImageUrl, CSSClass = "manufacturer-image" }).ToArray();
             }
             else
             {
@@ -70,7 +83,7 @@ namespace BlazorWebAssembyDemoApp.Client.Pages
 
         void SelectManufacturer(int manufacturerId)
         {
-            if (SelectedManufacturer==null)
+            if (SelectedManufacturer == null)
             {
                 SelectedManufacturer = Manufacturers.First(x => x.Id == manufacturerId);
                 SelectedManufacturer.CSSClass = "manufacturer-image-selected";
@@ -85,6 +98,98 @@ namespace BlazorWebAssembyDemoApp.Client.Pages
                 SelectedManufacturer.CSSClass = "manufacturer-image";
                 SelectedManufacturer = Manufacturers.First(x => x.Id == manufacturerId);
                 SelectedManufacturer.CSSClass = "manufacturer-image-selected";
+            }
+        }
+
+        IEnumerable<LaptopCompareModel> GetSortedLaptops()
+        {
+            if (SelectedManufacturer == null)
+            {
+                switch (SortMethod)
+                {
+                    case SortMethods.None:
+                        foreach (var l in LaptopList)
+                        {
+                            yield return l;
+                        }
+                        break;
+                    case SortMethods.Price:
+                        foreach (var l in LaptopList.OrderBy(x => x.Price))
+                        {
+                            yield return l;
+                        }
+                        break;
+                    case SortMethods.Rating:
+                        foreach (var l in LaptopList.OrderBy(x => x.Rating))
+                        {
+                            yield return l;
+                        }
+                        break;
+                    case SortMethods.Name:
+                        foreach (var l in LaptopList.OrderBy(x => x.Name))
+                        {
+                            yield return l;
+                        }
+                        break;
+                    case SortMethods.Memory:
+                        foreach (var l in LaptopList.OrderBy(x => x.Memory))
+                        {
+                            yield return l;
+                        }
+                        break;
+                    case SortMethods.Ram:
+                        foreach (var l in LaptopList.OrderBy(x => x.Ram))
+                        {
+                            yield return l;
+                        }
+                        break;
+                    default:
+                        break;
+                }
+            }
+            else
+            {
+                switch (SortMethod)
+                {
+                    case SortMethods.None:
+                        foreach (var l in LaptopList.Where(x=>x.Manufacturer.Id==SelectedManufacturer.Id))
+                        {
+                            yield return l;
+                        }
+                        break;
+                    case SortMethods.Price:
+                        foreach (var l in LaptopList.Where(x => x.Manufacturer.Id == SelectedManufacturer.Id).OrderBy(x => x.Price))
+                        {
+                            yield return l;
+                        }
+                        break;
+                    case SortMethods.Rating:
+                        foreach (var l in LaptopList.Where(x => x.Manufacturer.Id == SelectedManufacturer.Id).OrderBy(x => x.Rating))
+                        {
+                            yield return l;
+                        }
+                        break;
+                    case SortMethods.Name:
+                        foreach (var l in LaptopList.Where(x => x.Manufacturer.Id == SelectedManufacturer.Id).OrderBy(x => x.Name))
+                        {
+                            yield return l;
+                        }
+                        break;
+                    case SortMethods.Memory:
+                        foreach (var l in LaptopList.Where(x => x.Manufacturer.Id == SelectedManufacturer.Id).OrderBy(x => x.Memory))
+                        {
+                            yield return l;
+                        }
+                        break;
+                    case SortMethods.Ram:
+                        foreach (var l in LaptopList.Where(x => x.Manufacturer.Id == SelectedManufacturer.Id).OrderBy(x => x.Ram))
+                        {
+                            yield return l;
+                        }
+                        break;
+                    default:
+                        break;
+                }
             }
         }
 
--- BlazorWebAssembyDemoApp/Shared/UserModel.cs
+++ BlazorWebAssembyDemoApp/Shared/UserModel.cs
@@ -1,10 +1,16 @@
-﻿namespace BlazorWebAssembyDemoApp.Shared
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorWebAssembyDemoApp.Shared
 {
     public class UserModel
     {
         public int Id { get; set; }
+        [Required]
         public string FirstName { get; set; }
+        [Required]
         public string LastName { get; set; }
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
     }
 }
